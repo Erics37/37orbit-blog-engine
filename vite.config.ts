ba26260d427923d921aa38551702a409e5cb687c
Eric37 <315074833@qq.com>
2025-12-18 17:19:25 +0800

    update frontend

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-        allowedHosts: true,
+        allowedHosts: ['api.37orbit.com'],
       },
       plugins: [react()],
       define: {
